8de7f8714755dbde7226c83a28bbc6c0b1702dac
Eren2727 <80910591+Eren2727@users.noreply.github.com>
2026-06-12 20:46:08 +0300

    Update masskick.js

--- src/commands/Moderation/masskick.js
+++ src/commands/Moderation/masskick.js
@@ -8,7 +8,7 @@ import { InteractionHelper } from '../../utils/interactionHelper.js';
 export default {
     data: new SlashCommandBuilder()
         .setName("masskick")
-        .setDescription("Kick multiple users from the server at once")
+        .setDescription("Tek seferde birden fazla kişiyi sunucudan atar.")
         .addStringOption(option =>
             option
                 .setName("users")
